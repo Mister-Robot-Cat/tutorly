c9511a28d1062bee6dfe2777d87e50b5318e7273
melishis <55026516+Mister-Robot-Cat@users.noreply.github.com>
2026-03-12 00:12:14 +0400

    init

---
+++ tutorly/src/main/java/com/main/tutorly/TutorlyApplication.java
@@ -0,0 +1,13 @@
+package com.main.tutorly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorlyApplication.class, args);
+	}
+
+}
